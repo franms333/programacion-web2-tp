5d0b54879d9bf27c800a8c663bea1b8baea54b5b
franms333 <franms099@gmail.com>
2021-06-26 21:55:42 -0300

    agregado el archivo de la base de datos

---
+++ produccion_web.sql
@@ -0,0 +1,236 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 27-06-2021 a las 02:54:28
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `produccion_web`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `brands`
+--
+
+CREATE TABLE `brands` (
+  `brand_id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `brands`
+--
+
+INSERT INTO `brands` (`brand_id`, `name`, `deleted_at`) VALUES
+(2, 'Rolex', NULL),
+(3, 'Bulgari', NULL),
+(4, 'Omega', NULL),
+(5, 'Casio', NULL),
+(6, 'prueba', '2021-05-02 22:47:54'),
+(7, 'prueba', '2021-05-02 22:48:28'),
+(8, 'Pulseras', '2021-05-02 23:30:24'),
+(9, 'Pulseras doradas', '2021-05-02 23:30:44');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categories`
+--
+
+CREATE TABLE `categories` (
+  `category_id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `categories`
+--
+
+INSERT INTO `categories` (`category_id`, `name`, `deleted_at`) VALUES
+(1, 'Relojes dorados', NULL),
+(2, 'Relojes plateados', NULL),
+(6, 'Pulseras', '2021-05-02 22:43:36'),
+(7, 'Pulseras', '2021-05-02 22:02:21'),
+(8, 'qwerqwer', '2021-05-02 22:31:19'),
+(9, 'Pulseras', '2021-05-02 22:45:28');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comments`
+--
+
+CREATE TABLE `comments` (
+  `comment_id` int(11) NOT NULL,
+  `user` varchar(100) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `stars` int(11) DEFAULT NULL,
+  `is_visible` tinyint(1) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `comments`
+--
+
+INSERT INTO `comments` (`comment_id`, `user`, `product_id`, `description`, `stars`, `is_visible`, `created_at`) VALUES
+(6, 'Francisco', 5, 'Está buenísimo!!', NULL, NULL, '2021-05-03 20:43:48'),
+(7, 'Francisco', 5, 'Buenardo', NULL, NULL, '2021-05-03 20:44:03'),
+(8, 'Francisco', 5, 'buenardo', NULL, NULL, '2021-05-03 20:44:19');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL DEFAULT 0,
+  `brand_id` int(11) NOT NULL DEFAULT 0,
+  `name` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `price` float NOT NULL DEFAULT 0,
+  `stock` int(11) NOT NULL DEFAULT 0,
+  `image` varchar(200) NOT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `products`
+--
+
+INSERT INTO `products` (`product_id`, `category_id`, `brand_id`, `name`, `description`, `price`, `stock`, `image`, `deleted_at`) VALUES
+(1, 1, 2, 'Pulseras', 'asdjfjasdjfjasdj', 123456, 12, '', '2021-05-03 19:25:38'),
+(2, 1, 2, 'Reloj nuevo', 'Esta es la descripción', 100000, 5, '', '2021-05-03 19:27:33'),
+(5, 1, 2, 'Reloj dorado', 'Nuevo reloj dorado', 80000, 6, 'omega_dorado_1.jpg', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `subcategories`
+--
+
+CREATE TABLE `subcategories` (
+  `subcategory_id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `deleted_at` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `first_name` varchar(200) DEFAULT NULL,
+  `last_name` varchar(200) DEFAULT NULL,
+  `email` varchar(200) DEFAULT NULL,
+  `password` varchar(200) DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`brand_id`);
+
+--
+-- Indices de la tabla `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indices de la tabla `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment_id`);
+
+--
+-- Indices de la tabla `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indices de la tabla `subcategories`
+--
+ALTER TABLE `subcategories`
+  ADD PRIMARY KEY (`subcategory_id`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
+--
+-- AUTO_INCREMENT de la tabla `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `subcategories`
+--
+ALTER TABLE `subcategories`
+  MODIFY `subcategory_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
